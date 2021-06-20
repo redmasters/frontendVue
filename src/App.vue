a395266c3b19f9b43c1b8b9a21d788060cc8c5d2
Erasmo Jr <erasmo.cjunior@gmail.com>
2021-06-20 13:19:09 -0300

    Consuming backend

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <Navbar/>
+     <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <Users/>
+   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
+import Users from './components/Users.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Navbar,
+    Users,
   }
 }
 </script>
@@ -23,6 +29,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
---
+++ src/components/Users.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <b-container>
+            <b-table striped hover :items="users"></b-table>
+        </b-container>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+const endpoint = 'https://powerful-journey-92355.herokuapp.com/'
+
+export default {
+    data() {
+        return {
+            users: []
+        }
+    },
+    created() {
+        axios.get(endpoint + 'api/')
+            .then(response => {
+                this.users = response.data;
+            })
+    }
+}
+
+</script>
